b35dac5f9fe490aa163daa284eb8d6206a658b4c
Benedict Benny <benedict.benny@byjus.com>
2022-12-15 17:02:48 +0530

    to add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Add from "./components/AddorEdit";
 import HomePage from "./components/HomePage";
 function App(){
     return(
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomePage/>}> </Route>
-            <Route path="/AddorEdit" element={<Add/>}></Route>
-        </Routes>
+        <HashRouter basename="/">
+            <Routes>
+                <Route path="/" element={<HomePage/>}> </Route>
+                <Route path="/AddorEdit" element={<Add/>}></Route>
+            </Routes>
+        </HashRouter>
     </BrowserRouter>)
 }
 export default App;
